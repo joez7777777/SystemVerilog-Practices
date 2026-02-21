082fac970085d8ae23374857a601af7c8f23aa89
Joe Z <jz.joezhang@outlook.com>
2026-02-21 12:15:42 -0800

    Update format

--- fifo/sync_fifo_msb.sv
+++ fifo/sync_fifo_msb.sv
@@ -1,41 +1,41 @@
 module sync_fifo_msb #(
-  parameter WIDTH=8,
-  parameter DEPTH=16
+    parameter WIDTH=8,
+    parameter DEPTH=16
 )  (
-  input logic 	  clk,
-  input logic 	  rst_n,
-  input logic	  wr_en,
-  input logic  rd_en,
-  input logic [WIDTH-1:0] wr_data,
-  output logic [WIDTH-1:0]  rd_data,
-  output logic full,
-  output logic empty
+    input logic 	            clk,
+    input logic 	            rst_n,
+    input logic	              wr_en,
+    input logic               rd_en,
+    input logic  [WIDTH-1:0]  wr_data,
+    output logic [WIDTH-1:0]  rd_data,
+    output logic              full,
+    output logic              empty
 );
-  localparam PTR_WIDTH = $clog2(DEPTH);
-  logic [WIDTH-1:0] mem [DEPTH-1:0];
-  logic [PTR_WIDTH:0] rd_ptr, wr_ptr;
-  
-  always @(posedge clk or negedge rst_n) begin
-    if (!rst_n) begin
-      rd_ptr <= {(PTR_WIDTH+1)}{1'b0};
-      wr_ptr <= {(PTR_WIDTH+1)}{1'b0};
-    end
-	else begin
-      if (wr_en && !full) begin
-        mem[wr_ptr[PTR_WIDTH-1:0]] <= wr_data;
-        wr_ptr <= wr_ptr + 1'b1;
+    localparam PTR_WIDTH = $clog2(DEPTH);
+    logic [WIDTH-1:0] mem [DEPTH-1:0];
+    logic [PTR_WIDTH:0] rd_ptr, wr_ptr;
+    
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            rd_ptr <= {(PTR_WIDTH+1)}{1'b0};
+            wr_ptr <= {(PTR_WIDTH+1)}{1'b0};
+        end
+    else begin
+        if (wr_en && !full) begin
+            mem[wr_ptr[PTR_WIDTH-1:0]] <= wr_data;
+            wr_ptr <= wr_ptr + 1'b1;
+        end
+        if (rd_en && !empty) begin
+            rd_data <= mem[rd_ptr[PTR_WIDTH-1:0]];
+            rd_ptr <= rd_ptr + 1'b1;
+        end
       end
-      if (rd_en && !empty) begin
-        rd_data <= mem[rd_ptr[PTR_WIDTH-1:0]];
-        rd_ptr <= rd_ptr + 1'b1;
-      end
     end
-  end
-  
-  always @(*) begin
-    empty = (wr_ptr == rd_ptr);
-    full = ((wr_ptr[PTR_WIDTH-1:0] == rd_ptr[PTR_WIDTH-1:0])) && (wr_ptr[PTR_WIDTH] != rd_ptr[PTR_WIDTH]);
-  end
+    
+    always @(*) begin
+        empty = (wr_ptr == rd_ptr);
+        full = ((wr_ptr[PTR_WIDTH-1:0] == rd_ptr[PTR_WIDTH-1:0])) && (wr_ptr[PTR_WIDTH] != rd_ptr[PTR_WIDTH]);
+    end
 
 endmodule
 
--- fifo/sync_fifo_msb_tb.sv
+++ fifo/sync_fifo_msb_tb.sv
@@ -1,53 +1,53 @@
 `timescale 1ns/100ps
 module tb();
-  reg clk, rst_n, wr_en, rd_en;
-  reg [7:0] wr_data;
-  wire [7:0] rd_data;
-  wire full, empty;
+    reg clk, rst_n, wr_en, rd_en;
+    reg [7:0] wr_data;
+    wire [7:0] rd_data;
+    wire full, empty;
   
-  initial begin
-  	#10 clk = 1'b0;
-    forever #5 clk = ~clk;
-  end
-  
-  sync_fifo_msb #(
-    .WIDTH	(8),
-    .DEPTH	(16)
-  ) dut (
-    .clk	(clk),
-    .rst_n	(rst_n),
-    .wr_en	(wr_en),
-    .rd_en	(rd_en),
-    .wr_data (wr_data),
-    .rd_data (rd_data),
-    .full	(full),
-    .empty	(empty)
-  );
-  
-  initial begin
-  	#0 begin
-      	rst_n = 1'b0;
-    	wr_en = 1'b0;
-        rd_en = 1'b0;
-      	wr_data = 16'b0;
+    initial begin
+        #10 clk = 1'b0;
+        forever #5 clk = ~clk;
     end
-    #10 begin
-    	rst_n = 1'b1;
-    end
-    for (reg i=0; i<16; i++) begin
-      @(posedge clk) begin 
-        wr_en = 1;
-        wr_data = i;
-      end
-    end
-    @(posedge clk) wr_en = 0;
+    
+    sync_fifo_msb #(
+        .WIDTH	(8),
+        .DEPTH	(16)
+    ) dut (
+        .clk	(clk),
+        .rst_n	(rst_n),
+        .wr_en	(wr_en),
+        .rd_en	(rd_en),
+        .wr_data (wr_data),
+        .rd_data (rd_data),
+        .full	(full),
+        .empty	(empty)
+    );
+    
+    initial begin
+        #0 begin
+              rst_n = 1'b0;
+              wr_en = 1'b0;
+              rd_en = 1'b0;
+              wr_data = 16'b0;
+        end
+        #10 begin
+            rst_n = 1'b1;
+        end
+        for (reg i=0; i<16; i++) begin
+            @(posedge clk) begin 
+                wr_en = 1;
+                wr_data = i;
+            end
+        end
+        @(posedge clk) wr_en = 0;
 
-#40 $finish;
-  end
-  
-  initial begin
-    $dumpfile("fifo.vcd");
-    $dumpvars();
-  end
+        #40 $finish;
+    end
+    
+    initial begin
+        $dumpfile("fifo.vcd");
+        $dumpvars();
+    end
   
 endmodule
